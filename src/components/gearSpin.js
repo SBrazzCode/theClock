326ed853b93443cc96551321a30867c3d7dcac93
CrippleTable <brazzoni12@hotmail.com>
2020-05-27 15:55:12 -0600

    modified:   gatsby-config.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/clock.module.css 	modified:   src/components/gearSpin.js 	deleted:    src/components/layout.css 	deleted:    src/components/layout.js 	deleted:    src/images/gatsby-astronaut.png 	deleted:    src/images/gatsby-icon.png 	deleted:    src/pages/404.js 	modified:   src/pages/index.js 	deleted:    src/pages/page-2.tsx

--- src/components/gearSpin.js
+++ src/components/gearSpin.js
@@ -59,16 +59,23 @@ export default class GearSpin extends React.Component {
 }
 
 function getRotation(time, numTeeth, wheelName) {
+
+  //isClockWise gets assigned 1 or -1. -1 if it is counter-clockwise
   const isClockwise = isClockWise(wheelName)
+
+  //the angle between each tooth
   const teethAngleGap = 360 / numTeeth
 
   //This will rotate until there's an overflow. This could be analogous to a grandfather 
   //clock being full unwound, and a solution like a re-wind button/mechanism
   //could be implemented. However, I decided that is behind the scope of this project.
   const currentRotation = (teethAngleGap * time) * isClockwise
+
   return { transform: `rotate(` + currentRotation + `deg)` }
 }
 
+//Could rethink this. Have two arrays/one array and use .contains?
+// i.e let clockWiseGears = {"FourthWheel", "SecondWheel"}
 function isClockWise(wheelName){
     switch (wheelName) {
         case "EscapeWheel":
