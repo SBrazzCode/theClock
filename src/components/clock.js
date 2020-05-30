923e0e40b7a8fb44e6f2e0cc12b0c5475dcc96df
CrippleTable <brazzoni12@hotmail.com>
2020-05-30 16:42:19 -0600

    modified:   gatsby-config.js 	modified:   package-lock.json 	modified:   package.json 	modified:   res/gears.json 	modified:   src/components/clock.js 	modified:   src/components/gear.js 	modified:   src/components/gearRotationLayer.js 	modified:   src/components/gearStyleLayer.js 	gatsby-node.js

--- src/components/clock.js
+++ src/components/clock.js
@@ -5,6 +5,9 @@ import Escapement from "./escapement.js"
 import stopButtonStyles from "./styles/stopButton.module.css"
 import startButtonStyles from "./styles/startButton.module.css"
 import "bootstrap/dist/css/bootstrap.css"
+import $ from "jquery"
+import { graphql, useStaticQuery } from "gatsby"
+
 require("bootstrap")
 
 export default class Clock extends React.Component {
@@ -13,7 +16,13 @@ export default class Clock extends React.Component {
     this.tick = this.tick.bind(this)
     this.stopTime = this.stopTime.bind(this)
     this.startTime = this.startTime.bind(this)
-    this.state = { time: 0, ticking: true } //time measured in seconds
+    this.state = {
+      time: 0, //time measured in seconds
+      ticking: true,
+      gears: [],
+      gearsLoaded: false,
+      error: null,
+    }
   }
 
   //On each tick increment time by 1 second each. Every components rotation
@@ -30,34 +39,121 @@ export default class Clock extends React.Component {
     this.setState({ ticking: true })
   }
 
+  componentDidMount() {
+    // fetch("./res/gears.json")
+    //   .then(res => res.json())
+    //   .then(
+    //     result => {
+    //       this.setState({
+    //         gearsLoaded: true,
+    //         gears: result.gears,
+    //       })
+    //     },
+    //     error => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         error,
+    //       })
+    //     }
+    //   )
+    // fetch("./res/gears.json")
+    //   .then(res => res.json())
+    //   .then(
+    //     result => {
+    //       console.log(result)
+    //     },
+    //     error => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         error,
+    //       })
+    //     }
+    //   )
+    // fetch("./res/packasdfdsfsfdse.json")
+    // .then(res => console.log(res))
+    // .then(
+    //   result => {
+    //     console.log(result)
+    //   },
+    //   error => {
+    //     this.setState({
+    //       isLoaded: true,
+    //       error,
+    //     })
+    //   }
+    // )
+    // let jef = 2
+    // jef += 2
+    // let data
+    // $.getJSON( "res/gears.json", function(data){
+    //   console.log(data)
+    // })
+    // const gatsbyRepoData = useStaticQuery(graphql`
+    //   query {
+    //     github {
+    //       repository(name: "theClock", owner: "crippledtable") {
+    //         id
+    //         nameWithOwner
+    //         url
+    //       }
+    //     }
+    //   }
+    // `)
+
+    //         {gatsbyRepoData.github.repository.nameWithOwner}
+
+    // const [gears, setGears] = useState()
+    // useEffect(() => {
+    //   // get data from GitHub api
+    fetch(`https://github.com/CrippledTable/theClock/res/gears.json`)
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        this.setState({ gears: resultData })
+      }) // set data for the number of stars
+    // }, [])
+    console.log(this.state.gears)
+  }
+
   render() {
-    const time = this.state.time
+    const { time, ticking, gears, gearsLoaded, error } = this.state
+    if (error) {
+      return (
+        <div>
+          Error loading gears
+          <br />
+          Error: {error.message}
+        </div>
+      )
+    } else if (!gearsLoaded) {
+      return <div>Loading Gears</div>
+    } else {
+      return (
+        <div className={clockStyles.clock}>
+          {/* {gears.map(gear => <Gear gearObject={gear} time={time}/>)} */}
+          {gears.map(gear => (
+            <Gear {...gear} time={time} />
+          ))}
 
-    return (
-      <div className={clockStyles.clock}>
-        <Gear numTeeth={30} wheelName="EscapeWheel" time={time} />
-        <Gear numTeeth={20} wheelName="FourthWheel" time={time} />
+          {/* <Gear numTeeth={30} wheelName="EscapeWheel" time={time} />
+          <Gear numTeeth={20} wheelName="FourthWheel" time={time} /> */}
 
-        <Escapement
-          onTick={this.tick}
-          ticking={this.state.ticking}
-          time={time}
-        />
+          <Escapement onTick={this.tick} ticking={ticking} time={time} />
 
-        <input
-          type="button"
-          onClick={this.stopTime}
-          value="Stop Time"
-          className={`${"btn btn-danger"}  ${stopButtonStyles.stopButton}`}
-        />
+          <input
+            type="button"
+            onClick={this.stopTime}
+            value="Stop Time"
+            className={`${"btn btn-danger"}  ${stopButtonStyles.stopButton}`}
+          />
 
-        <input
-          type="button"
-          onClick={this.startTime}
-          value="Start Time"
-          className={`${"btn btn-warning"}  ${startButtonStyles.startButton}`}
-        />
-      </div>
-    )
+          <input
+            type="button"
+            onClick={this.startTime}
+            value="Start Time"
+            className={`${"btn btn-warning"}  ${startButtonStyles.startButton}`}
+          />
+        </div>
+      )
+    }
   }
 }
