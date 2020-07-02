34e27c7ab4bccf9153100ecf3d2b81a28da15198
CrippledTable <58870815+CrippledTable@users.noreply.github.com>
2020-07-02 14:15:04 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `theClock`,
     description: `A deconstructed grandfather clock.`,
-    author: `@ShaunBrazzoni`,
+    author: `@CrippledTable`,
     pathPrefix: '/theClock',
   },
   plugins: [
